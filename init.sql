9e21557d5d2d5d0246fd177523a1f3fa42dcb7be
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-09-10 12:52:08 +0300

    Split data to different tables

--- init.sql
+++ init.sql
@@ -7,23 +7,56 @@ CREATE ROLE readonly;
 GRANT USAGE ON SCHEMA public TO readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
 
-CREATE USER mainnet with password 'password';
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mainnet;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mainnet;
-
-CREATE TABLE daily_statistics
+CREATE TABLE daily_transactions_count
 (
-    computed_for_timestamp     numeric(20, 0) NOT NULL,
-    transactions_count         numeric(20, 0) NOT NULL,
-    teragas_used               numeric(20, 0) NOT NULL,
-    deposit_amount             numeric(45, 0) NOT NULL,
-    new_accounts_count         numeric(20, 0) NOT NULL,
-    deleted_accounts_count     numeric(20, 0) NOT NULL,
-    active_accounts_count      numeric(20, 0) NOT NULL,
-    new_contracts_count        numeric(20, 0) NOT NULL,
-    new_unique_contracts_count numeric(20, 0) NOT NULL,
-    active_contracts_count     numeric(20, 0) NOT NULL
+    daily_timestamp    numeric(20, 0) PRIMARY KEY,
+    transactions_count numeric(20, 0) NOT NULL
 );
 
-ALTER TABLE ONLY daily_statistics
-    ADD CONSTRAINT daily_statistics_pkey PRIMARY KEY (computed_for_timestamp);
+CREATE TABLE daily_teragas_used
+(
+    daily_timestamp numeric(20, 0) PRIMARY KEY,
+    teragas_used    numeric(20, 0) NOT NULL
+);
+
+CREATE TABLE daily_deposit_amount
+(
+    daily_timestamp numeric(20, 0) PRIMARY KEY,
+    deposit_amount  numeric(45, 0) NOT NULL
+);
+
+CREATE TABLE daily_new_accounts_count
+(
+    daily_timestamp    numeric(20, 0) PRIMARY KEY,
+    new_accounts_count numeric(20, 0) NOT NULL
+);
+
+CREATE TABLE daily_deleted_accounts_count
+(
+    daily_timestamp        numeric(20, 0) PRIMARY KEY,
+    deleted_accounts_count numeric(20, 0) NOT NULL
+);
+
+CREATE TABLE daily_active_accounts_count
+(
+    daily_timestamp       numeric(20, 0) PRIMARY KEY,
+    active_accounts_count numeric(20, 0) NOT NULL
+);
+
+CREATE TABLE daily_new_contracts_count
+(
+    daily_timestamp     numeric(20, 0) PRIMARY KEY,
+    new_contracts_count numeric(20, 0) NOT NULL
+);
+
+CREATE TABLE daily_new_unique_contracts_count
+(
+    daily_timestamp            numeric(20, 0) PRIMARY KEY,
+    new_unique_contracts_count numeric(20, 0) NOT NULL
+);
+
+CREATE TABLE daily_active_contracts_count
+(
+    daily_timestamp        numeric(20, 0) PRIMARY KEY,
+    active_contracts_count numeric(20, 0) NOT NULL
+);
