9e3915b79caead99eb28eff96758e117cb4ce297
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-09-21 10:09:37 +0300

    Fix other stats after adding new structure
    Needs additional check!

--- statistics/db_tables/daily_deposit_amount.py
+++ statistics/db_tables/daily_deposit_amount.py
@@ -1,10 +1,10 @@
 import typing
 
 from . import DAY_LEN_SECONDS, daily_start_of_range
-from ..sql_statistics import SqlStatistics
+from ..periodic_statistics import PeriodicStatistics
 
 
-class DailyDepositAmount(SqlStatistics):
+class DailyDepositAmount(PeriodicStatistics):
     @property
     def sql_create_table(self):
         # For September 2021, the biggest value here is 10^34.
@@ -17,6 +17,12 @@ class DailyDepositAmount(SqlStatistics):
                 collected_for_day DATE PRIMARY KEY,
                 deposit_amount    numeric(50, 0) NOT NULL
             )
+        '''
+
+    @property
+    def sql_drop_table(self):
+        return '''
+            DROP TABLE IF EXISTS daily_deposit_amount
         '''
 
     @property
@@ -35,12 +41,28 @@ class DailyDepositAmount(SqlStatistics):
         '''
 
     @property
+    def sql_select_all(self):
+        return '''
+            SELECT
+                DATE_TRUNC('day', TO_TIMESTAMP(DIV(execution_outcomes.executed_in_block_timestamp, 1000 * 1000 * 1000))) AS date,
+                SUM((action_receipt_actions.args->>'deposit')::numeric) AS total_deposit_amount
+            FROM action_receipt_actions
+            JOIN execution_outcomes ON execution_outcomes.receipt_id = action_receipt_actions.receipt_id
+            JOIN receipts ON receipts.receipt_id = action_receipt_actions.receipt_id
+            WHERE receipts.predecessor_account_id != 'system'
+                AND action_receipt_actions.action_kind IN ('FUNCTION_CALL', 'TRANSFER')
+                AND (action_receipt_actions.args->>'deposit')::numeric > 0
+                AND execution_outcomes.status IN ('SUCCESS_VALUE', 'SUCCESS_RECEIPT_ID')
+                AND execution_outcomes.executed_in_block_timestamp < (CAST(EXTRACT(EPOCH FROM DATE_TRUNC('day', NOW())) AS bigint) * 1000 * 1000 * 1000)
+            GROUP BY date
+            ORDER BY date
+        '''
+
+    @property
     def sql_insert(self):
         return '''
-            INSERT INTO daily_deposit_amount VALUES (
-                %(time)s,
-                %(result)s
-            )
+            INSERT INTO daily_deposit_amount VALUES VALUES %s
+            ON CONFLICT DO NOTHING
         '''
 
     @property
