3a3f7c870026571e8bddd8a87a58b6da8b125e19
jschintz <james.schintz@gmail.com>
2021-11-30 16:44:35 -0500

    remove_daily_transactions_per_account

--- aggregations/__init__.py
+++ aggregations/__init__.py
@@ -15,9 +15,6 @@ from .db_tables.daily_outgoing_transactions_per_account_count import (
 from .db_tables.daily_receipts_per_contract_count import DailyReceiptsPerContractCount
 from .db_tables.daily_tokens_spent_on_fees import DailyTokensSpentOnFees
 from .db_tables.daily_transactions_count import DailyTransactionsCount
-from .db_tables.daily_transactions_per_account_count import (
-    DailyTransactionsPerAccountCount,
-)
 from .db_tables.deployed_contracts import DeployedContracts
 from .db_tables.weekly_active_accounts_count import WeeklyActiveAccountsCount
 from .db_tables.near_ecosystem_entities import NearEcosystemEntities
