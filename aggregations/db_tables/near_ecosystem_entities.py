c9086265c6183cccff0ad25e6bb44ab3fd7fd193
jschintz <james.schintz@gmail.com>
2021-12-03 15:31:57 -0500

    update to remove pands

--- aggregations/db_tables/near_ecosystem_entities.py
+++ aggregations/db_tables/near_ecosystem_entities.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import numpy as np
+import json,requests
 from ..sql_aggregations import SqlAggregations
 
 
@@ -9,19 +8,19 @@ class NearEcosystemEntities(SqlAggregations):
         return """
             CREATE TABLE IF NOT EXISTS near_ecosystem_entities
             (
-                slug        TEXT PRIMARY KEY,
-                title       TEXT,
-                oneliner    TEXT,      
-                website     TEXT, 
-                category    TEXT,    
-                status      TEXT, 
-                contract    TEXT,
-                logo        TEXT,
-                is_app      BOOLEAN,
-                is_nft      BOOLEAN,
-                is_guild    BOOLEAN,
-                is_defi     BOOLEAN,
-                is_dao      BOOLEAN  
+                slug         TEXT PRIMARY KEY
+                ,title       TEXT
+                ,oneliner    TEXT      
+                ,website     TEXT 
+                ,category    TEXT    
+                ,status      TEXT 
+                ,contract    TEXT
+                ,logo        TEXT
+                ,is_app      BOOLEAN
+                ,is_nft      BOOLEAN
+                ,is_guild    BOOLEAN
+                ,is_defi     BOOLEAN
+                ,is_dao      BOOLEAN  
                                                 
             )
         """
@@ -39,46 +38,49 @@ class NearEcosystemEntities(SqlAggregations):
     @property
     def sql_insert(self):
         return """
-            INSERT INTO near_ecosystem_entities VALUES %s
-            ON CONFLICT DO NOTHING;
-            UPDATE near_ecosystem_entities SET slug = NULL WHERE slug = 'NaN'
-            UPDATE near_ecosystem_entities SET title  = NULL WHERE  title  = 'NaN'
-            UPDATE near_ecosystem_entities SET oneliner = NULL WHERE oneliner = 'NaN'
-            UPDATE near_ecosystem_entities SET website  = NULL WHERE website  = 'NaN'
-            UPDATE near_ecosystem_entities SET category = NULL WHERE category = 'NaN'
-            UPDATE near_ecosystem_entities SET status = NULL WHERE status  = 'NaN'
-            UPDATE near_ecosystem_entities SET contract = NULL WHERE contract = 'NaN'
-            UPDATE near_ecosystem_entities SET logo = NULL WHERE logo = 'NaN'
+            INSERT INTO near_ecosystem_entities VALUES(%s %s %s %s %s %s %s %s %s %s %s, %s, %s, %s, %s, %s) 
+            ON CONFLICT DO NOTHING
         """
 
     def collect(self, requested_timestamp: int):
         url = "https://raw.githubusercontent.com/near/ecosystem/main/entities.json"
-        df = pd.read_json(url)
-        df1 = df[
-            [
-                "slug",
-                "title",
-                "oneliner",
-                "website",
-                "category",
-                "status",
-                "contract",
-                "logo",
-            ]
-        ]
+        url_js ="https://raw.githubusercontent.com/jschintz/ecosystem/main/entities.json"
+        r = requests.get(url_js).text
+        data = []
+        data = json.loads(r)
 
-        app = df1["category"].str.contains("app")
-        nft = df1["category"].str.contains("nft")
-        guild = df1["category"].str.contains("guild")
-        defi = df1["category"].str.contains("defi")
-        dao = df1["category"].str.contains("dao")
+    def store(self, parameters: list):
+        url = "https://raw.githubusercontent.com/near/ecosystem/main/entities.json"
+        url_js ="https://raw.githubusercontent.com/jschintz/ecosystem/main/entities.json"
+        r = requests.get(url).text
+        data = []
+        data = json.loads(r)
 
-        merged_df1 = df1.join(app, rsuffix=("_app"))
-        merged_df2 = merged_df1.join(nft, rsuffix=("_nft"))
-        merged_df3 = merged_df2.join(guild, rsuffix=("_guild"))
-        merged_df4 = merged_df3.join(guild, rsuffix=("_defi"))
-        merged_df5 = merged_df4.join(guild, rsuffix=("_dao"))
+        sql_insert = """
+            INSERT INTO near_ecosystem_entities VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
+            ON CONFLICT DO NOTHING
+        """
 
-        nan = merged_df5.fillna("")
-        output_values = nan.values
-        return output_values.tolist()
+        with self.analytics_connection.cursor() as analytics_cursor:
+            #Remove & update schema
+            try:
+                analytics_cursor.execute(self.sql_drop_table)
+                self.analytics_connection.commit()
+            except psycopg2.errors.UndefinedTable:
+                self.analytics_connection.rollback()
+            try:
+                analytics_cursor.execute(self.sql_create_table)
+                self.analytics_connection.commit()
+            except psycopg2.errors.DuplicateTable:
+                self.analytics_connection.rollback()
+
+
+            for record in data:
+                app = ('app' in record['category'])    
+                nft = ('nft' in record['category'])  
+                guild = ('guild' in record['category'])  
+                defi = ('defi' in record['category'])  
+                dao = ('dao' in record['category']) 
+                analytics_cursor.execute(
+                    sql_insert, [record.get('slug'), record.get('title'), record.get('oneliner'), record.get('website'), record.get('category'), record.get('status'), record.get('contract'), record.get('logo'), app, nft, guild, defi, dao]
+                )
