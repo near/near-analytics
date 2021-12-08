2a88c4f3bde4268b9dbb559b43e170928ac3dab7
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-12-08 16:42:10 +0300

    fix: rework near_ecosystem_entities for NF

--- aggregations/db_tables/near_ecosystem_entities.py
+++ aggregations/db_tables/near_ecosystem_entities.py
@@ -1,4 +1,6 @@
-import json, requests
+import json
+import requests
+
 from ..sql_aggregations import SqlAggregations
 
 
@@ -8,20 +10,19 @@ class NearEcosystemEntities(SqlAggregations):
         return """
             CREATE TABLE IF NOT EXISTS near_ecosystem_entities
             (
-                slug         TEXT PRIMARY KEY
-                ,title       TEXT
-                ,oneliner    TEXT      
-                ,website     TEXT 
-                ,category    TEXT    
-                ,status      TEXT 
-                ,contract    TEXT
-                ,logo        TEXT
-                ,is_app      BOOLEAN
-                ,is_nft      BOOLEAN
-                ,is_guild    BOOLEAN
-                ,is_defi     BOOLEAN
-                ,is_dao      BOOLEAN  
-                                                
+                slug     TEXT PRIMARY KEY,
+                title    TEXT,
+                oneliner TEXT,
+                website  TEXT,
+                category TEXT,
+                status   TEXT,
+                contract TEXT,
+                logo     TEXT,
+                is_app   BOOLEAN,
+                is_nft   BOOLEAN,
+                is_guild BOOLEAN,
+                is_defi  BOOLEAN,
+                is_dao   BOOLEAN
             )
         """
 
@@ -33,63 +34,40 @@ class NearEcosystemEntities(SqlAggregations):
 
     @property
     def sql_select(self):
-        pass
+        raise NotImplementedError(
+            "No requests to Indexer DB needed for near_ecosystem_entities"
+        )
 
     @property
     def sql_insert(self):
         return """
-            INSERT INTO near_ecosystem_entities VALUES(%s %s %s %s %s %s %s %s %s %s %s, %s, %s, %s, %s, %s) 
+            INSERT INTO near_ecosystem_entities VALUES %s
             ON CONFLICT DO NOTHING
         """
 
-    def collect(self, requested_timestamp: int):
-        pass
+    def collect(self, requested_timestamp: int) -> list:
+        # Dirty hack to enforce the DB rewrite all the data each time
+        self.drop_table()
+        self.create_table()
 
-    def store(self, parameters: list):
         url = "https://raw.githubusercontent.com/near/ecosystem/main/entities.json"
-        r = requests.get(url).text
-        data = []
-        data = json.loads(r)
+        data = json.loads(requests.get(url).text)
 
-        sql_insert = """
-            INSERT INTO near_ecosystem_entities VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
-            ON CONFLICT DO NOTHING
-        """
-
-        with self.analytics_connection.cursor() as analytics_cursor:
-            # Remove & update schema
-            try:
-                analytics_cursor.execute(self.sql_drop_table)
-                self.analytics_connection.commit()
-            except psycopg2.errors.UndefinedTable:
-                self.analytics_connection.rollback()
-            try:
-                analytics_cursor.execute(self.sql_create_table)
-                self.analytics_connection.commit()
-            except psycopg2.errors.DuplicateTable:
-                self.analytics_connection.rollback()
-
-            for record in data:
-                app = "app" in record["category"]
-                nft = "nft" in record["category"]
-                guild = "guild" in record["category"]
-                defi = "defi" in record["category"]
-                dao = "dao" in record["category"]
-                analytics_cursor.execute(
-                    sql_insert,
-                    [
-                        record.get("slug"),
-                        record.get("title"),
-                        record.get("oneliner"),
-                        record.get("website"),
-                        record.get("category"),
-                        record.get("status"),
-                        record.get("contract"),
-                        record.get("logo"),
-                        app,
-                        nft,
-                        guild,
-                        defi,
-                        dao,
-                    ],
-                )
+        return [
+            [
+                record.get("slug"),
+                record.get("title"),
+                record.get("oneliner"),
+                record.get("website"),
+                record.get("category"),
+                record.get("status"),
+                record.get("contract"),
+                record.get("logo"),
+                "app" in record["category"],
+                "nft" in record["category"],
+                "guild" in record["category"],
+                "defi" in record["category"],
+                "dao" in record["category"],
+            ]
+            for record in data
+        ]
